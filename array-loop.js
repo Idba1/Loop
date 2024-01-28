695a9e1939ff60d13cd7c9c4b0c2f61b54fae209
Idba1 <moniraislamself@gmail.com>
2024-01-29 04:11:26 +0600

    display every elements of an array

---
+++ array-loop.js
@@ -0,0 +1,15 @@
+/* var numbers = [22, 33, 44, 55, 66, 77, 88, 99, 99, 11, 33, 44];
+for (var i = 0; i < 7; i++){
+    var number = numbers[i];
+    console.log(number);
+} */
+
+
+
+
+
+var numbers = [22, 33, 44,55, 66, 77, 88, 99, 11, 33, 44];
+for (var i = 0; i < numbers.length; i++){
+    var number = numbers[i];
+    console.log(number);
+}
