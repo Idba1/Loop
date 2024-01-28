d9e8b21c2961b4d4d59b5f5ef97ea1d45fdfde91
Idba1 <moniraislamself@gmail.com>
2024-01-29 03:41:30 +0600

    basic for loop structure

---
+++ while-for-loop.js
@@ -0,0 +1,21 @@
+var roastGiven = 0;
+while (roastGiven < 7){
+    console.log(roastGiven);
+    console.log("Roast den please");
+    roastGiven++;
+}
+
+
+
+
+// for loop
+for(var roastGiven = 0; roastGiven<10; roastGiven++){
+    console.log("Roast den please for loop");
+}
+
+
+// simple version of for loop
+for (var i = 0; i<10; i++){
+    console.log('simple version');
+}
+
--- while-loop.js
+++
@@ -1,6 +0,0 @@
-var roastGiven = 0;
-while (roastGiven < 7){
-    console.log(roastGiven);
-    console.log("Roast den please");
-    roastGiven++;
-}
